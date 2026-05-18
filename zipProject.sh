3810b1285735d394e0281f6ac1d970bfe399ea24
Peri Telemachou <pericles@outlook.com.au>
2026-05-18 17:59:50 +1000

    Add zipProject.sh for testing on another local machine, update .gitignore

---
+++ zipProject.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Clear out old `build/` directory
+setopt rmstarsilent
+rm -rf build/
+mkdir build/
+
+# Build frontend bundle
+if [ ! -d "./node_modules" ]; then
+    npm i
+fi
+npx webpack
+
+# Build frontend Go code
+GOOS=linux GOARCH=amd64 go build -o ./build/
+
+# Copy required files to `build/` directory
+mkdir -p build/_config
+cp -R _config/* build/_config/
+mkdir -p build/_data
+cp -R _data/* build/_data
+mkdir -p build/dist
+cp -R dist/* build/dist
+mkdir -p build/node_modules
+cp -R node_modules/* build/node_modules
+mkdir -p build/src
+cp -R src/* build/src
+cp *.json build/
+cp README.md build/
+cp secrets.txt build/
+cp tsconfig.json build/
+cp webpack.config.js build/
+cp .env build/
+cp ~/Desktop/thing.txt build/
+
+# Zip everything in build
+cd build && zip -r ../ownapi.zip *
+cd ..
