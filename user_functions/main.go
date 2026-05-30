fc1c7c06860672f2f567466c2c05ca29fb7b68d5
Peri Telemachou <pericles@outlook.com.au>
2026-05-30 18:37:49 +1000

    Add new user_functions directory with README, main.go and generated/ for code gen

---
+++ user_functions/main.go
@@ -0,0 +1,7 @@
+package main
+
+// NOTE: This file will be ignored for the purposes of file generation, feel free to use main() to test out your functions
+//
+// You can also add utilities here that you don't want included in the generated code
+func main() {
+}
